8c1cae8be8b9fc29a1b86630f26399efcd5b862f
Olawale Mustapha <walemust28@gmail.com>
2021-09-20 16:18:06 +0100

    Recent download addition

--- src/Components/About.js
+++ src/Components/About.js
@@ -47,7 +47,7 @@ const About = ({ data }) => {
             </div>
             <div className="columns download">
               <p>
-                <a href={resumeDownload} className="button" download="Olawale's Resume.pdf">
+                <a href={resumeDownload} download className="button">
                   <i className="fa fa-download"></i>Download Resume
                 </a>
               </p>
